ef31f6d136e2910cf167be5db4907ee06f22b12c
Denzel Washington .O. <denzelwash97@gmail.com>
2026-08-30 13:51:00 +0300

    Load security portal styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './security-home.css'
+import './security-portal.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
